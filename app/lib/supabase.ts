b2f3645baf3c3844e7a496d556b3685785287e77
nam090064-star <nam090064@gmail.com>
2026-08-12 09:23:39 +0700

    Fix supabase URL path error

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
+// Lưu ý: Không có dấu / ở cuối URL
 const supabaseUrl = 'https://rbccufsvvatyumynhdvd.supabase.co/rest/v1/'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJiY2N1ZnN2dmF0eXVteW5oZHZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYyNzA3MzAsImV4cCI6MjEwMTg0NjczMH0.FCU4Cqn-jLnQ5mQZy6xX-wBiWna5W_j9f--ZFfX6Ii4'
 
